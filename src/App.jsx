66296e1519b0e9a0832d6e1c3c4ab0b88cbb4a4c
Aparna Singha <aparna.24bcs10353@sst.scaler.com>
2025-04-02 16:12:00 +0530

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Landing from "./pages/landing/landing";
+import Auth from "./pages/landing/auth";
+import Creator from "./pages/dashboards/creator";
+import Worker from "./pages/dashboards/worker";
+import Assignment from "./pages/tools/assignment";
+import Answered from "./pages/assignments/Answered";
+
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<Auth />} />
+
+          <Route path="/dashboards/creator" element={<Creator />} />
+          <Route path="/dashboards/worker" element={<Worker />} />
+
+          <Route path="/tools/assigment" element={<Assignment />} />
+
+          <Route path="/assigment/:id" element={<Answered />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/dashboards/Worker.jsx
@@ -0,0 +1,5 @@
+export default function Worker() {
+  return (
+    <></>
+  );
+}
